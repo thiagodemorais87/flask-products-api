8badf9993f98169f3a40ccb574fc4511f916d960
Thiagosenai87 <thiago.goncalves@sistemafiepe.org.br>
2026-07-21 15:04:08 -0300

    feat: adiciona integracao com PostgreSQL via Docker Compose e persistencia de dados

--- app/config.py
+++ app/config.py
@@ -2,5 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "sua_chave_secreta_aqui")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
+    
+    # Substituído 'sqlite:///database.db' pela string de conexão do PostgreSQL do Docker
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", 
+        "postgresql://root:rootpassword@localhost:5439/products_db"
+    )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
